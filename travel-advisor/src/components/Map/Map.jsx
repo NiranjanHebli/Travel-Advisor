fc15daf8ffec8fe9011248d3e66608c5aec519bb
Niranjan <niranjanhebli77@gmail.com>
2023-02-19 01:22:09 +0530

    Basic Frontend done

---
+++ travel-advisor/src/components/Map/Map.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab";
+
+import useStyles from "./styles.js";
+
+const Map = () => {
+  const isMobile = useMediaQuery("(min-width:600px)");
+  const classes = useStyles();
+  const coordinates = { lat: 0, lng: 0 };
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyDmvFu9eyd3VKQYYJpKtfT0z4Nc0HuqLW4" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={""}
+        onChildClick={""}
+      ></GoogleMapReact>
+    </div>
+  );
+};
+
+export default Map;
---
+++ travel-advisor/src/components/PlaceDetails/PlaceDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PlaceDetails = ({places}) => {
+  return (
+    <div>
+      <h1>{places.name}</h1>
+    </div>
+  )
+}
+
+export default PlaceDetails
